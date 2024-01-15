bc0e0d79dd48ef0005b464e3d1c4ca58e1b12e30
Pedro <barbeitos.pedro@gmail.com>
2024-01-15 20:04:20 +0000

    navbar design updated

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,23 +1,23 @@
-import Link from 'next/link'
-import React from 'react'
-import './Navbar.scss'
+import Link from "next/link";
+import React from "react";
+import "./Navbar.scss";
 
-type Props = {}
+type Props = {};
 
 function Navbar({}: Props) {
   return (
-    <div className='navbar'>
-        <div className='navbarLeft'>
-            <Link href='/'><strong>Temples</strong></Link>
-            <Link href='/list'>Index</Link>
-            <button className='search'>Search</button>
-        </div>
-        <div className='navbarRight'>
-            <Link href='/shop'>Shop</Link>
-            <Link href='/about'>About</Link>
-        </div>
+    <div className="navbar">
+      <div className="navbarLeft">
+        <strong>Temples</strong>
+        <div>Index</div>
+        <div>Search</div>
+      </div>
+      <div className="navbarRight">
+        <div>Shop</div>
+        <div>About</div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
